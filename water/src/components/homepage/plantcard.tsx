1a634f738581bd9eed91751a532fdfc02bf40b26
Alyssa Hanson <alyghanson@gmail.com>
2024-06-02 19:31:43 -0700

    fixed image text formatting bugs

--- water/src/components/homepage/plantcard.tsx
+++ water/src/components/homepage/plantcard.tsx
@@ -10,10 +10,15 @@ interface PlantCardProps {
 function PlantCard({plant, openModal}: PlantCardProps) {
     return (
       <div className="plant-card" onClick={openModal}>
+        <div className="image-container">
+        <img className ="plant-image" src={plant.inputPicture || "/images/default-plant.png"}></img>
+        </div>
+        
+        <div className="text-container">
         <div className="card-content">{plant.name}</div>
         <div className="card-content">I was last watered on: {plant.lastWatering.toLocaleDateString()}</div>
         <div className="card-content">I will need to be watered again on: {new Date(plant.lastWatering.getTime() + plant.waterFrequency * 7 * 24 * 60 * 60 * 1000).toLocaleDateString()}</div> 
-        <img className ="plant-image" src={plant.inputPicture || "/images/default-plant.png"}></img>
+        </div>
       </div>
     );
   }
